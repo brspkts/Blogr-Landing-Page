6ba91ade0b67b823ad608d814247010f28f22fd9
brspkts <122319070+brspkts@users.noreply.github.com>
2023-05-02 00:38:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function product(){
+    const toggle1 = document.getElementById("toggle1")
+    if(toggle1.style.display === "none"){
+        toggle1.style.display = "flex"
+    }else{
+        toggle1.style.display = "none"
+    }
+}
+
+function company(){
+    const toggle2 = document.getElementById("toggle2")
+    if(toggle2.style.display === "none"){
+        toggle2.style.display = "flex"
+    }else{
+        toggle2.style.display = "none"
+    }
+}
+
+function connect(){
+    const toggle3 = document.getElementById("toggle3")
+    if(toggle3.style.display === "none"){
+        toggle3.style.display = "flex"
+    }else{
+        toggle3.style.display = "none"
+    }
+}
